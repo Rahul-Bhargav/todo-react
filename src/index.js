847f6698e9bc70b7045f1f0abb087aafefcfaec7
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-03-01 20:04:02 +0530

    refactor routes

--- src/index.js
+++ src/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import './index.css'
-import { Router, Route, IndexRoute, hashHistory } from 'react-router'
+import { Router, Route, hashHistory } from 'react-router'
 
 ReactDOM.render(
   <Router history={hashHistory}>
-    <Route path='/' component={App}>
-      <Route path=':location' component={App} />
-      <IndexRoute component={App} />
+    <Route path='/(:location)' component={App}>
     </Route>
   </Router>,
   document.getElementById('root')

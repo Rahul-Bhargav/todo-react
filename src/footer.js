218f7b9bfcd7293a80cd55c37a9dd063b49d6b5e
Rahul Surabhi <rahul.surabhi09@gmail.com>
2017-03-01 20:03:49 +0530

    change footer component to be functional

--- src/footer.js
+++ src/footer.js
@@ -1,19 +1,19 @@
 import React from 'react'
 import { Link, IndexLink } from 'react-router'
 
-export default class Footer extends React.Component {
-  render () {
-    const footerClass = this.props.showFooter ? 'footer' : 'footer footer-hide'
-    return (
-      <footer className={footerClass} id="todoapp-footer">
-        <span className="todo-count" id="todo-count">{this.props.todoCount + ' items completed'}</span>
-        <ul className="filters">
-          <li><IndexLink activeClassName='selected' to='/'>All</IndexLink></li>
-          <li><Link activeClassName='selected' to='/Active'>Active</Link></li>
-          <li><Link activeClassName='selected' to='/Completed'>Completed</Link></li>
-        </ul>
-        <button className="clear-completed-show" id="clear-completed">Clear completed</button>
-      </footer>
-    )
-  }
+const footer = (props) => {
+  const footerClass = props.showFooter ? 'footer' : 'footer footer-hide'
+  return (
+    <footer className={footerClass} id="todoapp-footer">
+      <span className="todo-count" id="todo-count">{props.todoCount + ' items completed'}</span>
+      <ul className="filters">
+        <li><IndexLink activeClassName='selected' to='/'>All</IndexLink></li>
+        <li><Link activeClassName='selected' to='/Active'>Active</Link></li>
+        <li><Link activeClassName='selected' to='/Completed'>Completed</Link></li>
+      </ul>
+      <button className="clear-completed-show" id="clear-completed" onClick={props.clearCompleted}>Clear completed</button>
+    </footer>
+  )
 }
+
+export default footer
